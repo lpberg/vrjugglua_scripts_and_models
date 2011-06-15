e24add3599f6323c9b00b9a90cb5c05c5cd41414
Leif Berg <lpberg@iastate.edu>
2011-06-15 12:46:46 -0500

    playing with lightings

--- examples/lys/lettherebelight.lua
+++ examples/lys/lettherebelight.lua
@@ -1,60 +1,66 @@
+require("Actions")
+dofile("X:/users/lpberg/VRJuggLua/examples/movetools.lua")
+light = {}
+lightGroup = osg.Group()
+RelativeTo.World:addChild(lightGroup)
+ss = RelativeTo.Room:getOrCreateStateSet()
+
+robot = Transform{
+	Model([[X:\users\adamc17\Factory\Factory Models\OSG\Machines\Robot.osg]])
+}
+Actions.addFrameAction(Rotation.createRotation(robot,"y",40))
+
 factory = Transform{
 		orientation = AngleAxis(Degrees(-90), Axis{1.0, 0.0, 0.0}),
 		scale = ScaleFrom.inches,
 		Model("X:/Users/lpberg/VRJuggLua/models/basicFactory.ive"),
 }
-
+RelativeTo.World:addChild(robot)
 RelativeTo.World:addChild(factory)
 
 
-light = {}
 function addLight(arg)
 	local l = osg.Light()
 	l:setLightNum(arg.number)
 	light[arg.number] = l
-	
+	l:setDirection(osg.Vec3(0,-1,0))
+	l:setSpotCutoff(60.0)
+	l:setSpotExponent(0.5)
 	local ls = osg.LightSource()
 	ls:setLight(l)
 	ls:setLocalStateSetModes(osg.StateAttribute.Values.ON)
-	
 	l:setAmbient(osg.Vec4(arg.ambient, arg.ambient, arg.ambient, 1))
 	l:setDiffuse(osg.Vec4(arg.diffuse, arg.diffuse, arg.diffuse, 1))
 	l:setSpecular(osg.Vec4(arg.specular, arg.specular, arg.specular, 1))
-	
 	l:setPosition(arg.position)
+	RelativeTo.World:addChild(Sphere{radius=.25,position={arg.position:x(),arg.position:y(),arg.position:z()}})
 	return ls
 end
-	
+	 
+--local dir = osg.Vec3f(0, 0, -.8)
+--dir:normalize()
 
-lightGroup = osg.Group()
--- Maybe need to be added to room instaed of world
-RelativeTo.World:addChild(lightGroup)
+light0 = addLight{
+	number = 0,
+	ambient = 0.8,
+	diffuse = 0.7,
+	specular = 0.0,
+	position = osg.Vec4(0, 4, 0, 1),
+	direction = dir
+}
 
-ss = RelativeTo.Room:getOrCreateStateSet()
+lightGroup:addChild(light0)
+--[[
+light1 = addLight{
+	number = 1,
+	ambient = 0.0,
+	diffuse = 0.5,
+	specular = 0.1,
+	position = osg.Vec4(0, 2, 2, 0),
+	--direction = osg.Vec3f(0, -1, 0)
+}
 
-local dir = osg.Vec3f(0, -1, 0)
-dir:normalize()
-
-lightGroup:addChild(
-	addLight{
-		number = 0,
-		ambient = 0.8,
-		diffuse = 0.7,
-		specular = 0.0,
-		position = osg.Vec4(0, 3, 2, 1),
-		direction = dir
-	}
-)
-ss:setAssociatedModes(light[0], osg.StateAttribute.Values.ON)
-
-lightGroup:addChild(
-	addLight{
-		number = 1,
-		ambient = 0.0,
-		diffuse = 0.5,
-		specular = 0.1,
-		position = osg.Vec4(0, 2, 2, 0),
-		direction = osg.Vec3f(0, -1, 0)
-	}
-)
-ss:setAssociatedModes(light[1], osg.StateAttribute.Values.ON)
+--lightGroup:addChild(light1)
+]]--
+--ss:setAssociatedModes(light[0], osg.StateAttribute.Values.ON)
+--ss:setAssociatedModes(light[1], osg.StateAttribute.Values.ON)
