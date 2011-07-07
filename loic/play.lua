349c3f92bf979a5216f660f24b5113a3548a6f37
Leif Berg <lpberg@iastate.edu>
2011-07-07 11:00:27 -0500

    peg and hole objects example : play

--- loic/play.lua
+++ loic/play.lua
@@ -8,15 +8,19 @@ vrjLua.appendToModelSearchPath(vrjLua.findInModelSearchPath("../models/"))
 
 factory = Transform{
 	orientation = AngleAxis(Degrees(-90), Axis{1.0, 0.0, 0.0}),
-	scale = ScaleFrom.inches,
+	scale = ScaleFrom.inches*2,
 	Model("basicfactory.ive")
 }
-RelativeTo.World:addChild(factory)
 
 peg = Transform{
+	position = {0,1,0},
 	Model("Pin.osg"),
 }
 hole = Transform{
-	Model("Hole.osg"),
+	Model("Block.osg"),
 }
+
+RelativeTo.World:addChild(factory)
+RelativeTo.World:addChild(peg)
+RelativeTo.World:addChild(hole)
 
