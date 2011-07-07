5e4e66dc750f4893022c525eee15d53bfd32e25e
Leif Berg <lpberg@iastate.edu>
2011-07-07 13:17:11 -0500

    added a light in simpleLIghts

--- examples/lys/simpleLights.lua
+++ examples/lys/simpleLights.lua
@@ -12,7 +12,7 @@ factory = Transform{
 --RelativeTo.World:addChild(factory)
 
 ss = RelativeTo.World:getOrCreateStateSet()
-RelativeTo.World:addChild(Sphere{radius=.23, position = {0,3,-5}})
+--RelativeTo.World:addChild(Sphere{radius=.23, position = {0,3,-5}})
 --RelativeTo.World:addChild(Sphere{radius=.23, position = {1.5,2,-6}})
 function doLight1()
 
@@ -31,16 +31,20 @@ end
 -- doLight1()
 function doLight1_5()
 
-	local l1 = osg.Light()
-	l1:setAmbient(osg.Vec4(.8, .8, 0.7, 1.0))
-	local ls1 = osg.LightSource()
-	ls1:setLight(l1)
-	ls1:setLocalStateSetModes(osg.StateAttribute.Values.ON)
-	ss:setAssociatedModes(l1, osg.StateAttribute.Values.ON)
+	l3 = osg.Light()
+	l3:setLightNum(3)
+	l3:setAmbient(osg.Vec4(.5, .5, .6, 1.0))
+	
+	ls3 = osg.LightSource()
+	ls3:setLight(l3)
+	ls3:setLocalStateSetModes(osg.StateAttribute.Values.ON)
+
+	ss:setAssociatedModes(l3, osg.StateAttribute.Values.ON)
+	
 	RelativeTo.Room:addChild(
-		ls1
+		ls3
 	)
-	l1:setPosition(osg.Vec4(1.5, 2, -6, 0))
+	l3:setPosition(osg.Vec4(0.0, 0.0, 1.25, 1.0))
 end
 function doLight2()
 
@@ -57,8 +61,9 @@ function doLight2()
 	RelativeTo.Room:addChild(
 		ls2
 	)
-	l2:setPosition(osg.Vec4(1.5, 2, -6, 1.0))
+	l2:setPosition(osg.Vec4(1.5, 2, 0, 1.0))
 end
 
 doLight1()
 doLight2()
+doLight1_5()
