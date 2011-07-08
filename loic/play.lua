aaba53855072c8e9def1460689e1560eb8f945b0
Leif Berg <lpberg@iastate.edu>
2011-07-08 08:33:10 -0500

    played with matrix transfrom matrices

--- loic/play.lua
+++ loic/play.lua
@@ -1,14 +1,15 @@
 -- Look for models in the same directory as this file.
+require("Actions")
 require("getScriptFilename")
 fn = getScriptFilename()
-assert(fn, "Have to load this from file, not copy and paste, or we can't find our models!")
 vrjLua.appendToModelSearchPath(fn)
 dofile(vrjLua.findInModelSearchPath("../examples/lys/simpleLights.lua"))
+dofile(vrjLua.findInModelSearchPath("../examples/movetools.lua"))
 vrjLua.appendToModelSearchPath(vrjLua.findInModelSearchPath("../models/"))
 
 factory = Transform{
 	orientation = AngleAxis(Degrees(-90), Axis{1.0, 0.0, 0.0}),
-	scale = ScaleFrom.inches*2,
+	scale = ScaleFrom.inches,
 	Model("basicfactory.ive")
 }
 
@@ -19,8 +20,32 @@ peg = Transform{
 hole = Transform{
 	Model("Block.osg"),
 }
+PAT = Transform{
+	position = {0,1,0},
+}
+obj1 = osg.MatrixTransform()
+obj1:addChild(hole)
+obj2 = osg.MatrixTransform()
+obj2:addChild(peg)
+TM = osg.MatrixTransform()
+TM:addChild(PAT)
+PAT:addChild(obj2)
 
-RelativeTo.World:addChild(factory)
-RelativeTo.World:addChild(peg)
-RelativeTo.World:addChild(hole)
+RelativeTo.World:addChild(obj1)
+RelativeTo.World:addChild(TM)
+
+rotateBlock = function()
+	local r = Rotation.rotate(obj1,"z",-45,45)
+	r()
+	obj1_preMatrix = obj1:getMatrix()
+
+	TM:preMult(obj1_preMatrix)
+	Actions.waitForRedraw()
+end
+
+Actions.addFrameAction(rotateBlock)
+
+
+
+
 
