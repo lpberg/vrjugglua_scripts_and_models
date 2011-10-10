ff93dd2288faca6ed5bf2e7e13bc0569a565a46a
= <=>
2011-10-10 11:15:16 -0500

    created drawing program from ryans examples

---
+++ examples/drawRainbowApp.lua
@@ -0,0 +1,57 @@
+--drawRainbowApp.lua - simple line drawing program using a device and button
+
+require("Actions")
+
+drawNewLine = function(lineWidth)
+	local geom = osg.Geometry()
+	local geode = osg.Geode()
+	geode:addDrawable(geom)
+	RelativeTo.World:addChild(geode)
+	local vertices = osg.Vec3Array()
+	geom:setVertexArray(vertices)
+	local colors = osg.Vec4Array()
+	geom:setColorArray(colors)
+	geom:setColorBinding(osg.Geometry.AttributeBinding.BIND_PER_VERTEX)
+	local linestrip = osg.DrawArrays(osg.PrimitiveSet.Mode.LINE_STRIP)
+	geom:addPrimitiveSet(linestrip)
+	-- setting line width
+	local stateRoot = geom:getOrCreateStateSet()
+	local lw = osg.LineWidth(lineWidth)
+	stateRoot:setAttribute(lw)
+	return vertices,colors,linestrip,geom
+end
+	
+Actions.addFrameAction(
+	function()
+		local drawBtn = gadget.DigitalInterface("VJButton2")
+		local device = gadget.PositionInterface("VJWand")
+		
+		getColor = coroutine.wrap(function()
+			while true do
+				coroutine.yield(osg.Vec4(1, 0, 0, 1))
+				coroutine.yield(osg.Vec4(0, 1, 0, 1))
+				coroutine.yield(osg.Vec4(0, 0, 1, 1))
+			end
+		end)
+		
+		function addPoint(v,vertices,colors,linestrip,geom)
+			vertices.Item:insert(v)
+			colors.Item:insert(getColor())
+			linestrip:setCount(#(vertices.Item))
+			geom:setVertexArray(vertices)
+		end
+		
+		while true do
+			repeat
+				Actions.waitForRedraw()
+			until drawBtn.justPressed
+				local width = 10 --math.random(5,20)
+				local verticies, colors, linestrip, geom = drawNewLine(width)
+			repeat
+				local pos = device.position
+				addPoint(osg.Vec3(pos:x(),pos:y(),pos:z()),verticies,colors,linestrip,geom)
+				Actions.waitForRedraw()
+			until not(drawBtn.pressed)
+		end
+	end
+)
