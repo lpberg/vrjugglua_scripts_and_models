2bd2807d8c3434662725085cc13ac39d8327b117
= <=>
2011-11-28 20:07:14 -0600

    added models for vr proj

--- VRProject/spinningteapot.lua
+++ VRProject/spinningteapot.lua
@@ -3,8 +3,16 @@ require("StockModels")
 require("Actions")
 dofile([[C:\Users\lpberg\Dropbox\Vance_Research\VRJuggLua\examples\older\movetools.lua]])
 
+s = 5
+
+
 sbox = Transform{
-	Sphere{radius=.23},
+	Transform{
+	position = {10,0,0},
+	--Sphere{radius=.23},
+	Model([[C:\Users\lpberg\Desktop\core.ive]])
+	--StockModels.Teapot(),
+	}
 }
 RelativeTo.World:addChild(sbox)
-Actions.addFrameAction(Transformation.oscillateZ(sbox,1,-2,1))
+Actions.addFrameAction(Rotation.createRotation(sbox,"y",25))
