bcf008a6a92ce4800b3a6a832e8ae5caf7a823ab
Leif Berg <lpberg@iastate.edu>
2011-12-08 20:06:21 -0600

    fixed up some functions in graphing area

--- CaveGraph/graph.lua
+++ CaveGraph/graph.lua
@@ -10,6 +10,7 @@ dofile(vrjLua.findInModelSearchPath([[holodeck/loadHolodeck.lua]]))
 strechBtn = gadget.DigitalInterface("VJButton1")
 dragBtn = gadget.DigitalInterface("VJButton2")
 spinBtn = gadget.DigitalInterface("WMButtonMinus")
+spinBtn2 = gadget.DigitalInterface("WMButtonPlus")
 
 device = gadget.PositionInterface("VJWand")
 hand = gadget.PositionInterface("VJHand")
@@ -17,35 +18,37 @@ hand = gadget.PositionInterface("VJHand")
 
 
 g = Graph()
-g:addNode(Node(osg.Vec3d(0,0,0),.125))
-g:addNode(Node(osg.Vec3d(1,0,0),.125))
-g:addNode(Node(osg.Vec3d(-1,0,0),.125))
-g:addNode(Node(osg.Vec3d(0,0,-1),.125))
-g:addNode(Node(osg.Vec3d(0,0,1),.125))
-g:addNode(Node(osg.Vec3d(0,1,0),.125))
-g:addNode(Node(osg.Vec3d(0,-1,0),.125))
-g:addEdge(g:getNode(0),g:getNode(1))
-g:addEdge(g:getNode(1),g:getNode(2))
-g:addEdge(g:getNode(2),g:getNode(3))
-g:addEdge(g:getNode(3),g:getNode(4))
-g:addEdge(g:getNode(4),g:getNode(5))
-g:addEdge(g:getNode(5),g:getNode(6))
+
+for i = 1, 20, 1 do
+	local x = math.random(-5,5)
+	local y = math.random(-5,5)
+	local z = math.random(-5,5)
+	g:addNode(Node(osg.Vec3d(x,y,z),.125))
+end
+	
+for i = 1, 20, 1 do
+	local fromNode = math.random(19)
+	local toNode = math.random(19)
+	if fromNode ~= toNode then
+		g:addEdge(g:getNode(fromNode),g:getNode(toNode))
+	end
+end
+
 RelativeTo.World:addChild(g.xform)
 
--- stateSet = g.xform:getOrCreateStateSet()
--- stateSet:setMode(0x0B50,osg.StateAttribute.Values.OFF)
+stateSet = g.xform:getOrCreateStateSet()
+stateSet:setMode(0x0B50,osg.StateAttribute.Values.OFF)
 
 Actions.addFrameAction(function()
 	while true do
 		while not dragBtn.pressed do
 			Actions.waitForRedraw()
 		end
-		print("button pressed")
 		while dragBtn.pressed do
 			local startPos = device.position
 			Actions.waitForRedraw()
 			local newVec = device.position
-			g:setPosition(g.xform:getPosition()+(-2*(startPos-newVec)))
+			g:setPosition(g.xform:getPosition()+(-3*(startPos-newVec)))
 			Actions.waitForRedraw()
 		end
 		
@@ -78,7 +81,7 @@ Actions.addFrameAction(function()
 end)
 
 
-rotFunc = function(dt) 
+spinFunc = function(dt) 
 	local calculateRot = function(aPos,bPos)
 		local deltaX = aPos:x() - bPos:x()
 		local deltaY = aPos:y() - bPos:y()
@@ -92,7 +95,6 @@ rotFunc = function(dt)
 
 	local angle = 0
 	local q = osg.Quat()
-	--local wandInit = device.position
 	while true do
 		while not spinBtn.pressed do
 			dt = Actions.waitForRedraw()
@@ -107,10 +109,39 @@ rotFunc = function(dt)
 	end
 end
 
-Actions.addFrameAction(rotFunc)
+Actions.addFrameAction(spinFunc)
 
 
 
+spinFunc2 = function(dt) 
+	local calculateRot = function(aPos,bPos)
+		local deltaX = aPos:x() - bPos:x()
+		local deltaY = aPos:y() - bPos:y()
+		local sign
+		if deltaY ~= 0 then sign = (deltaY/math.abs(deltaY)) or 1 else sign = 0 end
+		local forceFactorY = 10
+		local forceFactorX = 10
+		local force = math.abs(deltaY)*sign*forceFactorX*forceFactorY*(1/math.abs(deltaX))
+		return force
+	end
+
+	local angle = 0
+	local q = osg.Quat()
+	while true do
+		while not spinBtn2.pressed do
+			dt = Actions.waitForRedraw()
+		end
+		while spinBtn2.pressed do
+			local degree = calculateRot(device.position,hand.position)
+			angle = angle + degree * dt
+			q:makeRotate(Degrees(angle),  Axis{0,1,0})
+			g.xform:setAttitude(q)
+			dt = Actions.waitForRedraw()
+		end
+	end
+end
+
+Actions.addFrameAction(spinFunc2)
 
 
 
