fec683236377ab369c41684ee3ecb9a51d22e7ca
Leif Berg <lpberg@iastate.edu>
2011-12-14 13:45:23 -0600

    added a bunch of files for sound testing/playing

--- sound/tryme.lua
+++ sound/tryme.lua
@@ -1,18 +1,18 @@
 require("getScriptFilename")
 vrjLua.appendToModelSearchPath(getScriptFilename())
-o = vrjLua.findInModelSearchPath("tos-sulu-phasersready.mp3")
+mp3Path = vrjLua.findInModelSearchPath("tos-sulu-phasersready.mp3")
+ProgramReady = vrjLua.findInModelSearchPath("ProgramCompleteEnterWhenReady.wav")
+UpYourShaft = vrjLua.findInModelSearchPath("tos-scotty-upurshaft-fixed.wav")
 snx.changeAPI("OpenAL")
 --snx.changeAPI("Audiere")
 
 i = snx.SoundInfo()
-i.filename = vrjLua.findInModelSearchPath("tos-scotty-upurshaft-fixed.wav")
-i.ambient = false
+i.filename = ProgramReady
+i.ambient = true
 
 s = snx.SoundHandle(i.filename)
 --
-s:setPosition(-200,0,0)
 s:configure(i)
 do
-	s:trigger(1)
 	s:trigger(1)
 end
