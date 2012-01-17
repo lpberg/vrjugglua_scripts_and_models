52f40d968c4d90faa537ff38dfe36e4fb51b260c
Leif Berg <lpberg@iastate.edu>
2012-01-17 15:55:23 -0600

    added a spaceship model/script for light testing

---
+++ examples/lys/loadFactory.lua
@@ -0,0 +1,12 @@
+require("getScriptFilename")
+fn = getScriptFilename()
+assert(fn, "Have to load this from file, not copy and paste, or we can't find our models!")
+vrjLua.appendToModelSearchPath(fn)
+vrjLua.appendToModelSearchPath(vrjLua.findInModelSearchPath("../../models/"))
+
+factory = Transform{
+	orientation = AngleAxis(Degrees(-90), Axis{1.0, 0.0, 0.0}),
+	scale = ScaleFrom.inches,
+	Model("basicfactory.ive")
+}
+RelativeTo.World:addChild(factory)
