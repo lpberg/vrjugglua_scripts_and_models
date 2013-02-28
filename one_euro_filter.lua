0e6ccf86cd497a65f301d58bb742ffcf999c08ca
Leif Berg <lpberg@iastate.edu>
2013-02-28 09:38:02 -0600

    cleaned up low pass filter (one euro)

--- one_euro_filter.lua
+++ one_euro_filter.lua
@@ -1,11 +1,9 @@
-#Low Pass Filter (One Euro Filter) based on http://www.lifl.fr/~casiez/1euro/OneEuroFilter.py in Lua
+#Low Pass Filter (One Euro Filter) based on http: / / www.lifl.fr / ~casiez / 1euro / OneEuroFilter.py in Lua
 local LowPassFilterIndex = { isLowPassFilter = true}
 local LPFMT = { __index = LowPassFilterIndex }
 
-
 function LowPassFilterIndex:setAlpha(new_alpha)
 	assert(new_alpha >= 0 and new_alpha < 1.0)
-	-- change alpha to float?
 	self.alpha = new_alpha
 end
 
@@ -42,12 +40,12 @@ local OEFMT = { __index = OneEuroFilterIndex }
 
 
 function OneEuroFilterIndex:__alpha(cutoff)
-	te    = 1.0 / self.freq
-	tau   = 1.0 / (2*math.pi*cutoff)
-	return  1.0 / (1.0 + tau/te)
+	te = 1.0 / self.freq
+	tau = 1.0 / (2 * math.pi * cutoff)
+	return 1.0 / (1.0 + tau / te)
 end
 
-function OneEuroFilterIndex:callFunc(x,timestamp)
+function OneEuroFilterIndex:callFunc(x, timestamp)
 	---- update the sampling frequency based on timestamps
 	if self.lasttime and timestamp then
 		self.freq = 1.0 / (timestamp-self.lasttime)
@@ -56,13 +54,13 @@ function OneEuroFilterIndex:callFunc(x,timestamp)
 	---- estimate the current variation per second
 	local prev_x = self.x:lastValue()
 	if prev_x == nil then
-		dx = 0.0  
-	else 
-		dx = (x-prev_x)*self.freq
+		dx = 0.0
+	else
+		dx = (x-prev_x) * self.freq
 	end
 	edx = self.dx:callFunc{value = dx, timestamp = timestamp, alpha = self:__alpha(self.dcutoff)}
 	---- use it to update the cutoff frequency
-	local cutoff = self.mincutoff + self.beta*math.abs(edx)
+	local cutoff = self.mincutoff + self.beta * math.abs(edx)
 	---- filter the given value
 	return self.x:callFunc{value = x, timestamp = timestamp, alpha = self:__alpha(cutoff)}
 end
@@ -78,30 +76,29 @@ OneEuroFilter = function(filter) --__init__
 	return filter
 end
 
-function main()
-	local duration = 10.0 -- seconds
-    local noise_table = {}
-	local filtered_table = {}
-    
-    local f = OneEuroFilter{
-			freq = 120,       -- Hz
-			mincutoff =  1.0,  -- FIXME
-			beta = 1.0,       -- FIXME
-			dcutoff =  1.0    -- this one should be ok
-	}
-		
-    local timestamp = 0.0 --seconds
-	local steps = 0
-    while timestamp < duration do
-        signal = math.sin(timestamp)
-        noisy = signal + (math.random()-0.5)/5.0
-        filtered = f:callFunc(noisy, timestamp)
-		noise_table[steps] = noisy
-		filtered_table[steps] = filtered
-        timestamp = timestamp + 1.0/f.freq
-		steps = steps + 1
-	end
-	print("OneEuroFilter Complete")
-end
+--EXAMPLE USE (sin function)
+-- function mainSin()
+-- local duration = 10.0 -- seconds
+-- local noise_table = {}
+-- local filtered_table = {}
 
-main()
+-- local f = OneEuroFilter{
+-- freq = 120,       -- Hz
+-- mincutoff =  1.0,  -- FIXME
+-- beta = 1.0,       -- FIXME
+-- dcutoff =  1.0    -- this one should be ok
+-- }
+
+-- local timestamp = 0.0 --seconds
+-- local steps = 0
+-- while timestamp < duration do
+-- signal = math.sin(timestamp)
+-- noisy = signal + (math.random()-0.5)/5.0
+-- filtered = f:callFunc(noisy, timestamp)
+-- noise_table[steps] = noisy
+-- filtered_table[steps] = filtered
+-- timestamp = timestamp + 1.0/f.freq
+-- steps = steps + 1
+-- end
+-- print("OneEuroFilter Complete")
+-- end
