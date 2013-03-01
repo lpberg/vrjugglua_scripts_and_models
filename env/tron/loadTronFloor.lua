d70330ca6d25cc40b9c25887432bb903ddd4d61e
Leif Berg <lpberg@iastate.edu>
2013-03-01 16:11:24 -0600

    renamed, removed, and relocated file struction - major change

--- env/tron/loadTronFloor.lua
+++
@@ -1,6 +0,0 @@
-require("getScriptFilename")
-vrjLua.appendToModelSearchPath(getScriptFilename())
-print("loading Tron Floor, if not visible consider adding more lights to your scene!")
---Need better lighting as the model is a 2d Plane on X/Z
-dofile([[C:\Users\lpberg\Desktop\vrjugglua-3.0-vc9-2.8git\examples\env\simpleLights.lua]])
-RelativeTo.World:addChild(Transform{position = {.5,0,.5},Model("tron sketchy physics[1]~.osg"),})
