21792d74b9b48d49ebd5940db0462f7e2ab4030d
Leif Berg <lpberg@iastate.edu>
2013-03-01 16:12:22 -0600

    changed name of sound file

---
+++ sound/MySounds/sound_example.lua
@@ -0,0 +1,16 @@
+require("getScriptFilename")
+vrjLua.appendToModelSearchPath(getScriptFilename())
+dofile(vrjLua.findInModelSearchPath([[PlaySound.lua]]))
+
+-- mp3Path = vrjLua.findInModelSearchPath(PATH TO FILE HERE) 
+
+-- wavPath = vrjLua.findInModelSearchPath(PATH TO FILE HERE)
+
+-- myWavSound = SoundWav(wavPath)
+-- myMp3Sound = SoundMp3(mp3Path)
+-- myWavSound:trigger(1)
+-- myMp3Sound:trigger(1)
+
+INeedMoreCowbell()
+PlayStarTrekProgramCompleteMessage()
+
