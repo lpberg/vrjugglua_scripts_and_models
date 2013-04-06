4e4e5b4297b2c342e98eae3b99300b6c95c39dbc
Leif Berg <lpberg@iastate.edu>
2013-04-06 11:23:32 -0500

    changed startupScript to exclude group objects (messes with child transforms)

--- quickModelLoader/startupScript.lua
+++ quickModelLoader/startupScript.lua
@@ -1,39 +1,47 @@
 dofile([[X:\Users\lpberg\src\vrjugglua_scripts_and_models\useful_tools.lua]])
---default buttons in simSparta are RIGHT for select part, and B (trigger for drag/move)
-dofile([[X:\Users\lpberg\src\vrjugglua_scripts_and_models\simSparta.lua]])
+dofile([[X:\Users\lpberg\src\vrjugglua_scripts_and_models\SimSparta\SimSparta.lua]])
 
 loadBasicFactoryWithBasicLighting()
-
+local models = {}
 local global_position = {2,1.5,1.5}
-
-local main_xform = Transform{}
 
 local loadOSGsAndIves = function()
 	for i,v in pairs(arg) do
 		if string.find(v,".osg") or string.find(v,".ive") then
-			model_xform = Transform{position = global_position, Model(v)}
+			model = Transform{Model(v)}
+			model_xform = Transform{position = global_position, model}
+			table.insert(models,model)
 			changeTransformColor(model_xform,getRandomColor())
-			main_xform:addChild(createManipulatableObject(model_xform))
+			RelativeTo.World:addChild(createManipulatableObject(model_xform))
 		end
 	end
-	RelativeTo.World:addChild(createManipulatableObject(main_xform))
 end
-
 loadOSGsAndIves()
 
---add frame action for scaling objects 
+--set up buttons to change which object is "selected"
+nextBtn = gadget.DigitalInterface("WMButtonRight")
+prevBtn = gadget.DigitalInterface("WMButtonLeft")
+--set up button to move / drag object about scene
+dragBtn = gadget.DigitalInterface("WMButtonB")
+
+SimSparta(dragBtn,changeBtn,prevBtn)
+
+-- add frame action for scaling objects 
 Actions.addFrameAction(
 	function()
 		increaseBtn = gadget.DigitalInterface("WMButtonPlus")
 		decreaseBtn = gadget.DigitalInterface("WMButtonMinus")
 		while true do
 			if increaseBtn.justPressed then
-				scaleTransform(main_xform,main_xform:getScale():x()*1.05)
+				for _,v in ipairs(models) do
+					scaleTransform(v,v:getScale():x()*1.05)
+				end
 			end
 			if decreaseBtn.justPressed then
-				scaleTransform(main_xform,main_xform:getScale():x()*.95)
+				for _,v in ipairs(models) do
+					scaleTransform(v,v:getScale():x()*0.95)
+				end
 			end
 			Actions.waitForRedraw()
 		end
 	end)
--- 
