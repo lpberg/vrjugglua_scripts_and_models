1115b7373c71407d1393ef11c6a1b0449ede6464
Leif Berg <lpberg@iastate.edu>
2013-05-03 11:57:05 -0500

    added gnuplot interface to create graphs using Lua

---
+++ gnuplot/gnuplot.lua
@@ -0,0 +1,37 @@
+local PlotIndex = { isPlot = true}
+local PMT = { __index = PlotIndex }
+
+function PlotIndex:createCmd()
+	if self.plottype == "line" then
+		self.cmd = [[gnuplot -e "set terminal png size 5000,3000 background ']] .. self.bgcolor .. [['; set grid ytics lt 0; set autoscale; set title ']] .. self.title .. [[';set xlabel ']] .. self.xlabel .. [[' font 'Verdana, 18';set ylabel ']] .. self.ylabel .. [[' font 'Verdana, 18'; set output ']] .. self.outfile .. [['; plot '-' with linespoints title ']] .. self.title .. [[' linewidth ]] .. self.linewidth .. [[ lc rgb ']] .. self.color .. [[' pointsize 5 "; ]]
+	elseif self.plottype == "bar" then
+		self.cmd = [[gnuplot -e "set terminal png size 1600,900 background ']] .. self.bgcolor .. [[';set style fill solid;set grid ytics lt 0; set autoscale;set title ']] .. self.title .. [[';set xlabel ']] .. self.xlabel .. [[' font 'Verdana, 18';set ylabel ']] .. self.ylabel .. [[' font 'Verdana, 18'; set output ']] .. self.outfile .. [['; plot '-' using 2: xtic(1) with histogram lc rgb ']] .. self.color .. [[']]
+	else
+		print("Error: Must specifiy a chart type (line or bar)")
+	end
+end
+
+function PlotIndex:addRow(arg)
+	self.pipe:write(table.concat(arg, " "))
+	self.pipe:write("\n")
+end
+
+function PlotIndex:createImage()
+	self.pipe:close()
+end
+
+Plot = function(plot)
+	plot.color = plot.color or "#b5567b"
+	plot.xlabel = plot.xlabel or ""
+	plot.ylabel = plot.ylabel or ""
+	plot.linewidth = plot.linewidth or 5
+	plot.bgcolor = plot.bgcolor or '#e7e7e7'
+	plot.data_title = plot.data_title or ""
+	plot.outfile = plot.outfile or "defaultPlot.png"
+	plot.height = plot.height or 900
+	plot.width = plot.width or 900
+	setmetatable(plot, PMT)
+	plot:createCmd()
+	plot.pipe = assert(io.popen(plot.cmd, "w"))
+	return plot
+end
---
+++ gnuplot/plot_example.lua
@@ -0,0 +1,52 @@
+require("getScriptFilename")
+vrjLua.appendToModelSearchPath(getScriptFilename())
+dofile(vrjLua.findInModelSearchPath([[gnuplot.lua]]))
+
+math.randomseed(os.time())
+
+local filename = string.match(getScriptFilename(), "(.-)([^\\]-([^%.]+))$").."my"..".png"
+
+function lineExample()
+	--create plot
+	myplot = Plot{
+		color = "#a200ff",
+		plottype = "line",
+		title = "My Line Chart",
+		data_title = "My Data",
+		outfile = filename,
+		xlabel = "X-AXIS",
+		ylabel = "Y-AXIS",
+		linewidth = 3
+	}
+	--randomly add some data
+	for i = 1, 30 do
+		myplot:addRow{i, math.random(10)}
+	end
+	--create the image
+	myplot:createImage()
+end
+
+function barExample()
+	--create plot
+	myplot = Plot{
+		plottype = "bar",
+		title = "My Bar Chart",
+		data_title = "My Data",
+		outfile = "bar_chart.png",
+	}
+	--simple function to generate random strings 
+	function randName()
+		return string.char(math.random(65, 65 + 26), math.random(65, 65 + 26), math.random(65, 65 + 26), math.random(65, 65 + 26), math.random(65, 65 + 26))
+	end
+	--add some data
+	for i = 1, 10 do
+		myplot:addRow{randName(), math.random(2, 10)}
+	end
+	--create the image
+	myplot:createImage()
+end
+
+barExample()
+
+barExample()
+lineExample()
