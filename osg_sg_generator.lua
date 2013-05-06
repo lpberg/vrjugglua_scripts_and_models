14e7dcc23eeccbe8f27506c0733d5652a19b197b
Leif Berg <lpberg@iastate.edu>
2013-05-06 16:32:03 -0500

    started algorithm for visualizing OSG scene graph

---
+++ osg_sg_generator.lua
@@ -0,0 +1,19 @@
+
+
+
+
+local function visitNode(root)		
+	root_name = root:getName()
+	for i = 1,root:getNumChildren(),1 do
+		local child_name = root.Child[i]:getName()
+		--create childnode
+		--add edge between root and child
+	end
+	for i = 1,root:getNumChildren(),1 do
+		local child_name = root.Child[i]:getName()
+		--create childnode
+		--add edge between root and child
+	end
+end
+
+visitNode(root)
