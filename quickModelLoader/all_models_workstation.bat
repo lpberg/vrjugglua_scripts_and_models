13a771c27ed2b27b619757792a1048eb80b03074
unknown <lpberg@yoda.vrac.iastate.edu>
2013-12-02 08:29:54 -0600

    removed model loader scripts (extracted to its own repo for development)

--- quickModelLoader/all_models_workstation.bat
+++
@@ -1,4 +0,0 @@
-pushd "%~dp0"
-V:\Applications\vrjugglua\windows-snapshot\bin\NavTestbed.exe ^
-C:\Users\lpberg\src\vrjugglua_scripts_and_models\quickModelLoader\startupScript.lua  %*
-
--- quickModelLoader/drop_models_here_METaL_stereo.bat
+++
@@ -1,4 +0,0 @@
-pushd "%~dp0"
-V:\Applications\vrjugglua\windows-snapshot\bin\NavTestbed.exe ^
-s:/jconf30/METaL.tracked.stereo.reordered.withwand.jconf S:\jconf30\mixins\METaL.wiimotewandbuttons.jconf ^
-"S:\quick_model_loader\startupScript.lua"  %*
