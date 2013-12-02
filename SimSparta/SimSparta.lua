05480f78d3074ab7bce1057d24901ecd861ab766
unknown <lpberg@yoda.vrac.iastate.edu>
2013-12-02 14:12:47 -0600

    refactored SimSparta (into a fake Lua class) and example

--- SimSparta/SimSparta.lua
+++ SimSparta/SimSparta.lua
@@ -1,152 +1,178 @@
---Using Code from: https://gist.github.com/lpberg/5067603
-require("Actions")
 require("osgFX")
+local SimSpartaIndex = { isSimSparta = true }
+local SimSpartaMT = {__index = SimSpartaIndex}
 
-local Manipulables = {}
-local Manipulables_Switches = {}
-local initial_matricies = {}
-
-function createManipulatableObject(xform)
-
-	function wrapTransformInBlueScribeSwitch(xform)
-		local switch = osg.Switch()
-		local scribe = osgFX.Scribe()
-		scribe:setWireframeColor(osg.Vec4f(0, 0, 1, 1))
-		switch:addChild(xform)
-		switch:addChild(scribe)
-		scribe:addChild(xform)
-		switch:setSingleChildOn(0)
-		return switch
+function SimSpartaIndex:_setMatrix(xform, mat)
+	if osgLua.getTypeInfo(xform).name == [[osg::PositionAttitudeTransform]] then
+		xform:setPosition(mat:getTrans())
+		xform:setAttitude(mat:getRotate())
+	else
+		xform:setMatrix(mat)
 	end
-
-	function wrapTransformInMatrixTransform(xform)
-		local MT = osg.MatrixTransform()
-		MT:addChild(xform)
-		return MT
-	end
-
-	local xform_asMatrixTransform = wrapTransformInMatrixTransform(xform)
-	local xform_asScribeSwitch = wrapTransformInBlueScribeSwitch(xform_asMatrixTransform)
-	table.insert(Manipulables, xform_asMatrixTransform)
-	table.insert(initial_matricies, xform_asMatrixTransform:getMatrix())
-	table.insert(Manipulables_Switches, xform_asScribeSwitch)
-
-	return xform_asScribeSwitch
 end
 
-local getRoomToWorld = function()
-	return RelativeTo.World:getInverseMatrix()
+function SimSpartaIndex:_getMatrix(xform)
+	if osgLua.getTypeInfo(xform).name == [[osg::PositionAttitudeTransform]] then
+		mat = osg.Matrixd()
+		mat:setTrans(xform:getPosition())
+		mat:setRotate(xform:getAttitude())
+		return mat
+	elseif osgLua.getTypeInfo(xform).name == [[osg::MatrixTransform]] then
+		return xform:getMatrix()
+	end
 end
 
---- This is a HIDEOUS HACK @todo
-local matrixMult = function(a, b)
-	local copyOfA = osg.Matrixd(a)
-	copyOfA:preMult(b)
-	return copyOfA
+function SimSpartaIndex:_getPosition(xform)
+	mat = self:_getMatrix(xform)
+	return mat:getTrans()
 end
 
-local transformMatrixRoomToWorld = function(m)
-	return matrixMult(getRoomToWorld(), m)
+function SimSpartaIndex:_wrapTransformInBlueScribeSwitch(xform)
+	local switch = osg.Switch()
+	local scribe = osgFX.Scribe()
+	scribe:setWireframeColor(osg.Vec4f(0, 0, 1, 1))
+	switch:addChild(xform)
+	switch:addChild(scribe)
+	scribe:addChild(xform)
+	switch:setSingleChildOn(0)
+	return switch
 end
 
-local function moveAction(dragBtn, nextBtn, prevBtn, resetBtn)
+function SimSpartaIndex:_wrapTransformInMatrixTransform(xform)
+	MT = osg.MatrixTransform()
+	MT:addChild(xform)
+	return MT
+end
 
-	local wand = gadget.PositionInterface("VJWand")
-	local activeObject = 1
-
-	local getWandInWorld = function()
-		return transformMatrixRoomToWorld(wand.matrix)
-	end
-
-	local function getClosestObject()
-		Manipulables_Switches[activeObject]:setSingleChildOn(0)
-		local minDist = 100000
-		local closestObj = nil
-		local wandPos = getWandInWorld():getTrans()
-		for i = 1, #Manipulables, 1 do
-			local dist = (wandPos - Manipulables[i]:getMatrix():getTrans()):length()
-			if dist < minDist then
-				minDist = dist
-				activeObject = i
-			end
-		end
-		Manipulables_Switches[activeObject]:setSingleChildOn(1)
-	end
+function SimSpartaIndex:createManipulatableObject(xform)
+	assert(osgLua.getTypeInfo(xform).name == [[osg::MatrixTransform]] or osgLua.getTypeInfo(xform).name == [[osg::PositionAttitudeTransform]], "must pass PAT or MT")
+	local xform_asScribeSwitch = self:_wrapTransformInBlueScribeSwitch(xform)
+	table.insert(self.Manipulables, xform)
+	table.insert(self.initial_matricies, self:_getMatrix(xform))
+	table.insert(self.Manipulables_Switches, xform_asScribeSwitch)
+	self.parent:addChild(xform_asScribeSwitch)
+	return
+end
 
 
-	local function selectNextObject()
-		--turn off scribe effect current node
-		Manipulables_Switches[activeObject]:setSingleChildOn(0)
-		--set active object index increase by one
-		activeObject = activeObject + 1
-		--if obect index greater than number of objects reset to first
-		if activeObject > #Manipulables then
-			activeObject = 1
-		end
-		--set active object scribe on
-		Manipulables_Switches[activeObject]:setSingleChildOn(1)
-	end
+function SimSpartaIndex:_getWandInWorld()
+	return self.wand.matrix * RelativeTo.World:getInverseMatrix()
+end
 
-	local function selectPreviousObject()
-		--turn off scribe effect current node
-		Manipulables_Switches[activeObject]:setSingleChildOn(0)
-		--set active object index decrease by one
-		activeObject = activeObject - 1
-		--if object index less than 1 (0) reset to last
-		if activeObject < 1 then
-			activeObject = #Manipulables
-		end
-		--set active object scribe on
-		Manipulables_Switches[activeObject]:setSingleChildOn(1)
-	end
-
-	local function resetPart()
-		local node = Manipulables[activeObject]
-		node:setMatrix(initial_matricies[activeObject])
-	end
-
-	local function checkCycleButtons()
-		if nextBtn.justPressed then
-			selectNextObject()
-		end
-		if prevBtn ~= nil and prevBtn.justPressed then
-			selectPreviousObject()
-		end
-		if resetBtn ~= nil and resetBtn.justPressed then
-			resetPart()
+function SimSpartaIndex:_getClosestObject()
+	self.Manipulables_Switches[self.activeObject]:setSingleChildOn(0)
+	local minDist = 100000
+	local wandPos = self:_getWandInWorld():getTrans()
+	for i = 1, #self.Manipulables, 1 do
+		local manipulablePos = self:_getPosition(self.Manipulables[i])
+		local dist = (wandPos - manipulablePos):length()
+		if dist < minDist then
+			minDist = dist
+			self.activeObject = i
 		end
 	end
+	self.Manipulables_Switches[self.activeObject]:setSingleChildOn(1)
+end
 
-	local function frameAction()
-		while true do
-			while not dragBtn.pressed do
-				if cycle then
-					checkCycleButtons()
-				else
-					getClosestObject()
+function SimSpartaIndex:_selectNextObject()
+	--turn off scribe effect current node
+	self.Manipulables_Switches[self.activeObject]:setSingleChildOn(0)
+	--set active object index increase by one
+	self.activeObject = self.activeObject + 1
+	--if obect index greater than number of objects reset to first
+	if self.activeObject > #self.Manipulables then
+		self.activeObject = 1
+	end
+	--set active object scribe on
+	self.Manipulables_Switches[self.activeObject]:setSingleChildOn(1)
+end
+
+function SimSpartaIndex:_selectPreviousObject()
+	--turn off scribe effect current node
+	self.Manipulables_Switches[self.activeObject]:setSingleChildOn(0)
+	--set active object index increase by one
+	self.activeObject = self.activeObject - 1
+	--if obect index greater than number of objects reset to first
+	if self.activeObject < 1 then
+		self.activeObject = #self.Manipulables
+	end
+	--set active object scribe on
+	self.Manipulables_Switches[self.activeObject]:setSingleChildOn(1)
+end
+
+function SimSpartaIndex:_resetPart()
+	local node = self.Manipulables[self.activeObject]
+	self:_setMatrix(node, self.initial_matricies[self.activeObject])
+end
+
+function SimSpartaIndex:_checkCycleButtons()
+	if self.nextBtn.justPressed then
+		self:_selectNextObject()
+	end
+	if self.prevBtn ~= nil and self.prevBtn.justPressed then
+		self:_selectPreviousObject()
+	end
+	if self.resetBtn ~= nil and self.resetBtn.justPressed then
+		self:_resetPart()
+	end
+end
+
+function SimSpartaIndex:addFrameActions()
+	local function selectionFunc()
+		if self.cycleThroughParts then
+			self:_checkCycleButtons()
+		else
+			self:_getClosestObject()
+		end
+	end
+	Actions.addFrameAction(
+		function()
+			local nodeRelativeToWand = ""
+			local node = ""
+			while true do
+				selectionFunc()
+				if self.dragBtn.justPressed then
+					node = self.Manipulables[self.activeObject]
+					local wandpos = self:_getWandInWorld()
+					nodeRelativeToWand = self:_getMatrix(node) * osg.Matrixd.inverse(wandpos)
+				end
+				if self.dragBtn.pressed then
+					local new_mat = nodeRelativeToWand * self:_getWandInWorld()
+					self:_setMatrix(node, new_mat)
 				end
 				Actions.waitForRedraw()
 			end
-
-			local node = Manipulables[activeObject]
-			local node_matrix = node:getMatrix()
-			local nodeRelativeToWand = matrixMult(osg.Matrixd.inverse(getWandInWorld()), node_matrix) -- what is required
-
-			while dragBtn.pressed do
-				local new_mat = matrixMult(getWandInWorld(), nodeRelativeToWand)
-				node:setMatrix(new_mat)
-				Actions.waitForRedraw()
-			end
 		end
-	end
-	return frameAction
+	)
 end
 
-function SimSparta(dragBtn, nextBtn, prevBtn, resetBtn)
-	if dragBtn == nil or nextBtn == nil then
-		print("SimSparta: Must pass both valid dragBtn and nextBtn (optionally: prevBtn,resetBtn)")
-	else
-		Actions.addFrameAction(moveAction(dragBtn, nextBtn, prevBtn, resetBtn))
-		print("SimSparta: frame action initiated successfully")
+SimSparta = function(item)
+	--must have a drag button to manipulate geometry
+	assert(item.dragBtn, "SimSparta: must pass dragBtn for manipulating objects")
+	--if cycleThroughParts is true (not false) then make sure the other buttons are provided
+	if item.cycleThroughParts ~= false then
+		item.cycleThroughParts = true
+		assert(item.nextBtn, "SimSparta: must pass nextBtn for manipulating objects")
+		assert(item.prevBtn, "SimSparta: must pass prevBtn for manipulating objects")
 	end
+	--assume parent is RelativeTo.World if nothing passed
+	item.parent = item.parent or RelativeTo.World
+	--assume wand is VJWand unless passed
+	item.wand = item.wand or gadget.PositionInterface("VJWand")
+	--create internal data tables
+	item.Manipulables = {}
+	item.initial_matricies = {}
+	item.Manipulables_Switches = {}
+	--set active item index to be first item in table
+	item.activeObject = 1
+	--we must set the metatable - so it can find its methods
+	setmetatable(item, SimSpartaMT)
+	--add any passed nodes to data structures
+	for _, xform in ipairs(item) do
+		item:createManipulatableObject(xform)
+	end
+	--begin frame action
+	item:addFrameActions()
+	--return 'instance' of SimSparta
+	return item
 end
--- SimSparta/SimSparta_Example.lua
+++ SimSparta/SimSparta_Example.lua
@@ -1,26 +1,33 @@
---SimSparta Example
--- require("Actions")
-require("getScriptFilename")
-vrjLua.appendToModelSearchPath(getScriptFilename())
-dofile(vrjLua.findInModelSearchPath([[../simSparta.lua]]))
--- cycle=true -- optional cycle through method using buttons
---set up button to change which object is "selected"
-nextBtn = gadget.DigitalInterface("VJButton0")
-prevBtn = gadget.DigitalInterface("VJButton1") -- optional
---set up button to move / drag object about scene
-dragBtn = gadget.DigitalInterface("VJButton2")
+--SimSparta Example - simple geometry manipulation
+require("AddAppDirectory")
+AddAppDirectory()
+runfile[[SimSparta.lua]]
 
---add multiple objects as manipulatable objects
-local sphere1 = Transform{position = {0, 0, 0}, Sphere{radius = .125}}
-
-local sphere2 = Transform{position = {1, 0, 0}, Sphere{radius = .125}}
-
-local spheres = Transform{
-	createManipulatableObject(sphere1),
-	createManipulatableObject(sphere2),
+--create geometry to be manipulated
+local sphere1 = Transform{
+	position = {.5, 0, 0},
+	Sphere{radius = .125},
 }
---add objects to scene
-RelativeTo.World:addChild(createManipulatableObject(spheres))
 
---call SimSparta function to initiate frame action (prevBtn Optional)
-SimSparta(dragBtn, nextBtn, prevBtn, resetBtn)
+local sphere2 = MatrixTransform{
+	Sphere{radius = .125},
+}
+
+--create SimSparta frame action using proximity selection 
+SimSparta{
+	sphere1,
+	sphere2,
+	cycleThroughParts = false,
+	dragBtn = gadget.DigitalInterface("VJButton2"),
+}
+
+--create SimSparta frame action using cycle button selection 
+-- SimSparta{
+	-- sphere1,
+	-- sphere2,
+	-- cycleThroughParts = true,
+	-- dragBtn = gadget.DigitalInterface("VJButton2"),
+	-- nextBtn = gadget.DigitalInterface("VJButton0"),
+	-- prevBtn = gadget.DigitalInterface("VJButton1"),
+	-- resetBtn = gadget.DigitalInterface("VJButton4"),
+-- }
