2047182fae61690181a7d5db71244fff18c2f19f
unknown <lpberg@yoda.vrac.iastate.edu>
2013-12-02 08:30:28 -0600

    added simpleLights lua file in folder for testing with other apps

--- simpleLights.lua
+++ simpleLights.lua
@@ -9,7 +9,7 @@ function doLight1()
 	ls3:setLocalStateSetModes(osg.StateAttribute.Values.ON)
 	ss:setAssociatedModes(l3, osg.StateAttribute.Values.ON)
 	RelativeTo.Room:addChild(ls3)
-	l3:setPosition(osg.Vec4(-1.0, 0.0, 1.25, 1.0))
+	l3:setPosition(osg.Vec4(-1.0, 0.0, 10.25, 1.0))
 end
 
 function doLight2()
@@ -21,7 +21,7 @@ function doLight2()
 	ls2:setLocalStateSetModes(osg.StateAttribute.Values.ON)
 	ss:setAssociatedModes(l2, osg.StateAttribute.Values.ON)
 	RelativeTo.Room:addChild(ls2)
-	l2:setPosition(osg.Vec4(1.5, 2, 1.25, 1.0))
+	l2:setPosition(osg.Vec4(1.5, 20, 1.25, 1.0))
 end
 
 doLight1()
