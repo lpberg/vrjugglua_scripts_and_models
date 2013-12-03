b6d7915f16b6ec11cfb3e83c726e59fa36f24638
Leif Berg <lpberg@iastate.edu>
2013-12-03 14:19:12 -0600

    added scripts to create KiwiVR config files

---
+++ KiwiVR_example/kiwiVRConfigCreator.lua
@@ -0,0 +1,63 @@
+--lua script to create KiwiVR Config Files (xml)
+require("getScriptFilename")
+
+local _firstString = [[
+<?xml version="1.0"?>
+<MasterSettings xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <audioOptions>
+    <sourceID>-1</sourceID>
+    <recognizerEngineID>SR_MS_en-US_Kinect_11.0</recognizerEngineID>
+    <echoMode>None</echoMode>
+    <noiseSurpression>false</noiseSurpression>
+    <autoGainEnabled>false</autoGainEnabled>
+  </audioOptions>
+  <skeletonOptions>
+    <isSeatedMode>false</isSeatedMode>
+    <skeletonSortMode>OriginZClosest</skeletonSortMode>
+    <individualSkeletons />
+  </skeletonOptions>
+  <feedbackOptions>
+    <useFeedback>false</useFeedback>
+    <feedbackSensorNumber>0</feedbackSensorNumber>
+    <sensorJointType>HipCenter</sensorJointType>
+  </feedbackOptions>
+  <voiceButtonCommands />
+  <voiceTextCommands>
+]];
+local _secondString = [[
+  </voiceTextCommands>
+  <gestureCommands />
+  <kinectOptions />
+</MasterSettings>
+]];
+
+local function _makeMiddlePart(word,confidence,servername)
+	return [[
+		<VoiceTextCommand>
+		  <serverName>]]..servername..[[</serverName>
+		  <commandType>Voice</commandType>
+		  <comments>None</comments>
+		  <serverType>Text</serverType>
+		  <confidence>]]..confidence..[[</confidence>
+		  <recognizedWord>]]..word..[[</recognizedWord>
+		  <actionText>]]..word..[[</actionText>
+		</VoiceTextCommand>
+	]]
+end
+function createKiwiVRConfigFile(a)
+	assert(a.words, 'Must pass words.')
+	a.outfile = a.outfile or "outfile.xml"
+	a.filenameInThisDir = string.match(getScriptFilename(), "(.-)([^\\]-([^%.]+))$") .. a.outfile..".xml"
+	a.file = assert(io.open(a.filenameInThisDir, "w"))
+	a.servername = a.servername or "KVRPN_Tracker00"
+	filestr = _firstString
+	for _,t in ipairs(a.words) do
+		local nextPart = _makeMiddlePart(t[1],t[2],a.servername)
+		filestr = filestr .. nextPart
+	end
+	filestr = filestr .._secondString
+	a.file:write(filestr)
+	a.file:close()
+	print(a.filenameInThisDir.." created.")
+	return a.filenameInThisDir
+end
---
+++ KiwiVR_example/kiwiVRConfigCreator_example.lua
@@ -0,0 +1,17 @@
+--example of createKiwiVRConfigFile function 
+require("AddAppDirectory")
+AddAppDirectory()
+runfile[[kiwiVRConfigCreator.lua]]
+
+my_config_file = createKiwiVRConfigFile{
+	words = {
+		-- {"word",confidence},
+		{"hello",.85},
+		{"goodbye",.11},
+		{"dog",.75},
+		{"cat",.51},
+		{"bears",.81},
+	},
+	outfile = "myXML",
+	servername = "VRPN_Voice1",
+}
