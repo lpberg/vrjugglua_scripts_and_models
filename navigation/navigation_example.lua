25811a6382cdcdd586b43aa7ca1940c31a6ed970
Leif Berg <lpberg@iastate.edu>
2013-12-02 20:16:04 -0600

    overhauled FlyAndWalkNavigation to be just Navigation.
    added driving mode and easier way to switch between navigation modes using coroutines

--- navigation/navigation_example.lua
+++ navigation/navigation_example.lua
@@ -1,15 +1,24 @@
 -- navigation example
-require("Actions")
-require("getScriptFilename")
-vrjLua.appendToModelSearchPath(getScriptFilename())
-dofile(vrjLua.findInModelSearchPath([[navigation.lua]]))
+require("AddAppDirectory")
+AddAppDirectory()
+runfile[[navigation.lua]]
 
---add a Sphere as a reference point (to see if nav is working..
+--add a sphere as a reference point (to see navigation working..)
 RelativeTo.World:addChild(Sphere{})
 
---add navigation (optionally: specifiy buttons)
-myNav = FlyOrWalkNavigation{
-	start = "flying", --or "walking"
+--add navigation (optionally: specify buttons) - optional arguments commented out
+myNav = Navigation{
 	switchButton = gadget.DigitalInterface("VJButton1"),
 	initiateRotationButton1 = gadget.DigitalInterface("VJButton2"),		
+	-- start = "flying", -- (default) or "walking" or "driving"
+	-- initiateRotationButton2 = gadget.DigitalInterface("VJButton2"),		
+	-- moveButton = gadget.DigitalInterface("VJButton0") --default
+	-- dropToGroundWhenWalking = false --(default is true)
+	-- rate = 1.5 --(default)
+	-- rotRate = .5 --(default)
 }
+
+--optional methods
+-- myNav:switchToNavigation("driving")
+-- myNav:startRotating() --runs automatically at the start of the frame action
+-- myNav:stopRotating()
