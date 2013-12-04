ff6c3e4e921e9de7d93c1a35c70c3395280945b4
unknown <lpberg@yoda.vrac.iastate.edu>
2013-12-04 09:46:31 -0600

    minimized metal speech example

--- KiwiVR_example/METaLSpeechExample.lua
+++ KiwiVR_example/METaLSpeechExample.lua
@@ -2,28 +2,21 @@ require("AddAppDirectory")
 AddAppDirectory()
 runfile[[kiwiVRConfigCreator.lua]]
 
---- tips for using voice in METaL
--- 		make sure no Navtestbed processes are still running
--- 		RDP should be configured to leave audio on remote machine and pass plug and play devices (?)
--- 		when loading jconfs make sure to load the metal one you want as well
-
 --load in a configuration file to talk to the voice server
 --? - why do we have to load both, shouldn't launcher take care of one?
 vrjKernel.loadConfigFile(vrjLua.findInModelSearchPath([[VRPNText.jconf]]))
 vrjKernel.loadConfigFile(vrjLua.findInModelSearchPath([[s:/jconf30/METaL.tracked.stereo.reordered.withwandandhand.jconf]]))
 
---list of words to recognize with confidence numbers
-local mywords = {
-	{"hello",.85},
-	{"goodbye",.11},
-	{"dog",.75},
-	{"cat",.51},
-	{"bears",.81},
-}
-
---create the config file and store it on S: drive (for metal win7 server)
-my_config_file = createKiwiVRConfigFile{
-	words = mywords,
+--create the config file and store it on S: drive (for METaL win7 server)
+createKiwiVRConfigFile{
+	words = {
+		{"hello",.85},
+		{"goodbye",.75},
+		{"dog",.75},
+		{"cat",.75},
+		{"bears",.81},
+		{"load config files",.81},
+	},
 	fullPathFileName = [[S:\ProjectsCurrent\KiwiVRConfigFiles\custom.xml]],
 	servername = "Tracker00",
 }
