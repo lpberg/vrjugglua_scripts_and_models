23f815b82838efd1445be329b1347c25d3e0ff44
unknown <lpberg@yoda.vrac.iastate.edu>
2014-02-03 09:08:22 -0600

    updated frameActionSwitcher to include a onExit optional parameter for functions

--- frameActionSwitcher/frameActionSwitcher.lua
+++ frameActionSwitcher/frameActionSwitcher.lua
@@ -11,22 +11,35 @@ function frameActionSwitcherIndex:createNextFrameActionCoroutine()
 	end)
 end
 
+function frameActionSwitcherIndex:runOnExitFunctionForCurrentActionFrame(idx)
+	if idx ~= 0 then
+		local exitFunc = Actions.addFrameAction(self.frame_action_on_exit[idx])
+		while coroutine.status(exitFunc) ~= "dead" do
+			Actions.waitForRedraw()
+		end
+	end
+end
+
 function frameActionSwitcherIndex:switchToNextFrameAction()
+	self:runOnExitFunctionForCurrentActionFrame(self.previousActionFrameIdx)
 	Actions.removeFrameAction(self.current_frame_action_marker)
 	local _, nextActionFrameIdx = coroutine.resume(self.nextCo)
 	print("frameActionSwitcher: switched to "..self.frame_action_names[nextActionFrameIdx])
 	self.current_frame_action_marker = Actions.addFrameAction(self.frame_actions[nextActionFrameIdx])
+	self.previousActionFrameIdx = nextActionFrameIdx
 end
 
--- function frameActionSwitcherIndex:switchToFrameAction(faName)
-	-- Actions.removeFrameAction(self.current_frame_action_marker)
-	-- local nextActionFrameIdx = ""
-	-- repeat
-		-- _, nextActionFrameIdx = coroutine.resume(self.nextCo)
-	-- until self.frame_action_names[nextActionFrameIdx] == faName
-	-- print("frameActionSwitcher: switched to "..self.frame_action_names[nextActionFrameIdx])
-	-- self.current_frame_action_marker = Actions.addFrameAction(self.frame_actions[nextActionFrameIdx])
--- end
+function frameActionSwitcherIndex:switchToFrameAction(faName)
+	self:runOnExitFunctionForCurrentActionFrame(self.previousActionFrameIdx)
+	Actions.removeFrameAction(self.current_frame_action_marker)
+	local nextActionFrameIdx = ""
+	repeat
+		_, nextActionFrameIdx = coroutine.resume(self.nextCo)
+	until self.frame_action_names[nextActionFrameIdx] == faName
+	print("frameActionSwitcher: switched to "..self.frame_action_names[nextActionFrameIdx])
+	self.current_frame_action_marker = Actions.addFrameAction(self.frame_actions[nextActionFrameIdx])
+	self.previousActionFrameIdx = nextActionFrameIdx
+end
 
 function frameActionSwitcherIndex:addSwitchButtonActionFrame()
 	Actions.addFrameAction(function()
@@ -44,10 +57,17 @@ frameActionSwitcher = function(item)
 	-- item.current_frame_action_marker = ""
 	item.frame_actions = {}
 	item.frame_action_names = {}
+	item.frame_action_on_exit = {}
+	item.previousActionFrameIdx = 0
 	-- item.current_frame_action_marker = nil
 	for _, frameAction in ipairs(item) do
 		table.insert(item.frame_actions, frameAction[1])
 		table.insert(item.frame_action_names, frameAction[2])
+		if frameAction[3] then
+			table.insert(item.frame_action_on_exit, frameAction[3])
+		else
+			table.insert(item.frame_action_on_exit, function() return end)
+		end
 	end
 	item.nextCo = item:createNextFrameActionCoroutine()
 	--must start nav frame action before switch button?
--- frameActionSwitcher/frameActionSwitcherExample.lua
+++ frameActionSwitcher/frameActionSwitcherExample.lua
@@ -8,12 +8,22 @@ function print1()
 		Actions.waitForRedraw()
 	end
 end
+
+function print1_onExit()
+	print("exiting print1")
+end
+
 function print2()
 	while true do
 		print(2)
 		Actions.waitForRedraw()
 	end
 end
+
+function print2_onExit()
+	print("exiting print2")
+end
+
 function print3()
 	while true do
 		print(3)
@@ -21,9 +31,13 @@ function print3()
 	end
 end
 
+function print3_onExit()
+	print("exiting print3")
+end
+
 mySwitcher = frameActionSwitcher{
 	switchButton = gadget.DigitalInterface("VJButton2"),
-	{print1,"print 1"},
-	{print2,"print 2"},
-	{print3,"print 3"},
+	{print1,"print 1",print1_onExit},
+	{print2,"print 2",print2_onExit},
+	{print3,"print 3",print3_onExit},
 }
