c3efe2ce5b9a6a001394bce10076d72031b61cc7
Leif Berg <lpberg@iastate.edu>
2014-03-01 13:10:07 -0600

    updated example to represent OSG files

--- nodeSelector/nodeSelectorExample.lua
+++ nodeSelector/nodeSelectorExample.lua
@@ -4,24 +4,25 @@ runfile[[nodeSelector.lua]]
 
 local balls = Transform{
 	Transform{
-		Sphere{position = {-1, 0, 0},radius=.125},
-		Sphere{position = {-1,.5,0},radius=.125},
-		Sphere{position = {-1, 1, 0},radius=.125},
+		Transform{
+			Sphere{position = {-1, 0, 0},radius=.125},
+		},
+		Transform{
+			Sphere{position = {-1, .5, 0},radius=.125},
+		}
 	},
 	Transform{
-		Sphere{position = {0, 0, 0},radius=.125},
-		Sphere{position = {0,.5,0},radius=.125},
-		Sphere{position = {0, 1, 0},radius=.125},
+		Transform{
+			Transform{
+				Sphere{position = {0, 0, 0},radius=.125},
+			}
+		}
 	},
 	Transform{
 		Sphere{position = {1, 0, 0},radius=.125},
-		Sphere{position = {1,.5,0},radius=.125},
-		Sphere{position = {1, 1, 0},radius=.125},
 	},
 	Transform{
 		Sphere{position = {1.5, 0, 0},radius=.125},
-		Sphere{position = {1.5,.5,0},radius=.125},
-		Sphere{position = {1.5, 1, 0},radius=.125},
 	}
 }
 
@@ -31,6 +32,13 @@ ns = nodeSelector{
 	parent = balls,
 }
 
--- ns:moveDown()
--- ns:moveUp()
-
+local isParentTopofSnake = ns:isTopOfSnake(ns.parent)
+print("Should print false: ",isParentTopofSnake)
+local isChild1TopofSnake = ns:isTopOfSnake(ns.parent.Child[1])
+print("Should print false: ",isChild1TopofSnake)
+local isChild2TopofSnake = ns:isTopOfSnake(ns.parent.Child[2])
+print("Should print true: ",isChild2TopofSnake)
+local isChild3TopofSnake = ns:isTopOfSnake(ns.parent.Child[3])
+print("Should print true: ",isChild3TopofSnake)
+local isChild4TopofSnake = ns:isTopOfSnake(ns.parent.Child[4])
+print("Should print true: ",isChild4TopofSnake)
