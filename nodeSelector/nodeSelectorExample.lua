e22e06221280896b51c7398227247b286983e5a8
Leif Berg <lpberg@iastate.edu>
2014-03-02 09:33:31 -0600

    experimented with osg optimizer

--- nodeSelector/nodeSelectorExample.lua
+++ nodeSelector/nodeSelectorExample.lua
@@ -1,8 +1,12 @@
 require("AddAppDirectory")
+require("osgUtil")
 AddAppDirectory()
 runfile[[nodeSelector.lua]]
+runfile[[helper.lua]]
 
-local balls = Transform{
+op = osgUtil.Optimizer()
+
+balls = Transform{
 	Transform{
 		Transform{
 			Sphere{position = {-1, 0, 0},radius=.125},
@@ -28,17 +32,21 @@ local balls = Transform{
 
 RelativeTo.World:addChild(balls)
 
-ns = nodeSelector{
-	parent = balls,
-}
+printGraph(balls)
+op:optimize(balls,osgUtil.Optimizer.OptimizationOptions.ALL_OPTIMIZATIONS)
+print()
+printGraph(balls)
+-- ns = nodeSelector{
+	-- parent = balls,
+-- }
 
-local isParentTopofSnake = ns:isTopOfSnake(ns.parent)
-print("Should print false: ",isParentTopofSnake)
-local isChild1TopofSnake = ns:isTopOfSnake(ns.parent.Child[1])
-print("Should print false: ",isChild1TopofSnake)
-local isChild2TopofSnake = ns:isTopOfSnake(ns.parent.Child[2])
-print("Should print true: ",isChild2TopofSnake)
-local isChild3TopofSnake = ns:isTopOfSnake(ns.parent.Child[3])
-print("Should print true: ",isChild3TopofSnake)
-local isChild4TopofSnake = ns:isTopOfSnake(ns.parent.Child[4])
-print("Should print true: ",isChild4TopofSnake)
+-- local isParentTopofSnake = ns:isTopOfSnake(ns.parent)
+-- print("Should print false: ",isParentTopofSnake)
+-- local isChild1TopofSnake = ns:isTopOfSnake(ns.parent.Child[1])
+-- print("Should print false: ",isChild1TopofSnake)
+-- local isChild2TopofSnake = ns:isTopOfSnake(ns.parent.Child[2])
+-- print("Should print true: ",isChild2TopofSnake)
+-- local isChild3TopofSnake = ns:isTopOfSnake(ns.parent.Child[3])
+-- print("Should print true: ",isChild3TopofSnake)
+-- local isChild4TopofSnake = ns:isTopOfSnake(ns.parent.Child[4])
+-- print("Should print true: ",isChild4TopofSnake)
