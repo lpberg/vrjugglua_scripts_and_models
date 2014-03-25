31548745b5f46ac81ee883ae99fd2a70cfb93ebf
Leif Berg <lpberg@iastate.edu>
2014-03-25 08:07:28 -0500

    updated nodeSelector Example with Optimizer. I don't remember why I did this.

--- nodeSelector/nodeSelectorExample.lua
+++ nodeSelector/nodeSelectorExample.lua
@@ -4,9 +4,11 @@ AddAppDirectory()
 runfile[[nodeSelector.lua]]
 runfile[[helper.lua]]
 
-op = osgUtil.Optimizer()
-
 balls = Transform{
+	Transform{
+		scale = 1/100,
+		Model[[C:\Users\lpberg\Desktop\danfoss_pump.osg]]
+	},
 	Transform{
 		Transform{
 			Sphere{position = {-1, 0, 0},radius=.125},
@@ -32,13 +34,9 @@ balls = Transform{
 
 RelativeTo.World:addChild(balls)
 
-printGraph(balls)
-op:optimize(balls,osgUtil.Optimizer.OptimizationOptions.ALL_OPTIMIZATIONS)
-print()
-printGraph(balls)
--- ns = nodeSelector{
-	-- parent = balls,
--- }
+ns = nodeSelector{
+	parent = balls,
+}
 
 -- local isParentTopofSnake = ns:isTopOfSnake(ns.parent)
 -- print("Should print false: ",isParentTopofSnake)
@@ -50,3 +48,52 @@ printGraph(balls)
 -- print("Should print true: ",isChild3TopofSnake)
 -- local isChild4TopofSnake = ns:isTopOfSnake(ns.parent.Child[4])
 -- print("Should print true: ",isChild4TopofSnake)
+
+
+Actions.addFrameAction(function()
+	local wand = gadget.PositionInterface("VJWand")
+	local dragBtn = gadget.DigitalInterface("VJButton2")
+	
+	local function setMatrix(xform, mat)
+		if osgLua.getTypeInfo(xform).name == [[osg::PositionAttitudeTransform]] then
+			xform:setPosition(mat:getTrans())
+			xform:setAttitude(mat:getRotate())
+		else
+			xform:setMatrix(mat)
+		end
+	end
+
+	local function getMatrix(xform)
+		if osgLua.getTypeInfo(xform).name == [[osg::PositionAttitudeTransform]] then
+			mat = osg.Matrixd()
+			mat:setTrans(xform:getPosition())
+			mat:setRotate(xform:getAttitude())
+			return mat
+		elseif osgLua.getTypeInfo(xform).name == [[osg::MatrixTransform]] then
+			return xform:getMatrix()
+		end
+	end
+
+	local function getWandInWorld()
+		return wand.matrix * RelativeTo.World:getInverseMatrix()
+	end
+
+	local function getPosition(xform)
+		mat = getMatrix(xform)
+		return mat:getTrans()
+	end
+	
+	while true do
+		if dragBtn.justPressed then
+			node = ns.selectedNode
+			local wandpos = getWandInWorld()
+			nodeRelativeToWand = getMatrix(node) * osg.Matrixd.inverse(wandpos)
+		end
+		if dragBtn.pressed then
+			print(getPosition(node))
+			local new_mat = nodeRelativeToWand * getWandInWorld()
+			setMatrix(node, new_mat)
+		end
+		Actions.waitForRedraw()
+	end
+end)
