64b980d2e4538916c8356c50728a63cb37d685c8
Leif Berg <lpberg@iastate.edu>
2015-03-09 09:15:35 -0500

    major updates.
    moved models into folder. added lights. and more ships!

--- space/main.lua
+++ space/main.lua
@@ -1,6 +1,8 @@
 require("AddAppDirectory")
 AddAppDirectory()
 
+runfile[[simpleLights.lua]]
+
 local function CenterTransformAtPosition(xform, pos)
 	local bound = xform:getBound()
 	return Transform{
@@ -10,36 +12,46 @@ local function CenterTransformAtPosition(xform, pos)
 end
 
 local vortex = Transform{
-	Model[[13.ive]],
+	Model[[models/vortex.ive]],
 	orientation = AngleAxis(Degrees(30), Axis{1.0, 0.0, 0.0}),
 	scale = .01,
 }
-vortex = CenterTransformAtPosition(vortex,{0,0,0})
+vortex = CenterTransformAtPosition(vortex,{0,-5,-14})
 
-local soloarSystem = Transform{
-	Model[[solarsystem.ive]],
+local solarSystem = Transform{
+	Model[[models/solarsystem.ive]],
 	position = {5,0,-25},
 	orientation = AngleAxis(Degrees(-90), Axis{0.0, 1.0, 0.0}),
 	scale = 0.01,
 }
 
+local shuttle_sw = Transform{
+	Transform{
+		Model[[models/shuttle_sw.ive]],
+		orientation = AngleAxis(Degrees(-90), Axis{1.0, 0.0, 0.0}),
+	},
+	orientation = AngleAxis(Degrees(180), Axis{0.0, 1.0, 0.0}),
+	scale = .0001,
+}
+shuttle_sw = CenterTransformAtPosition(shuttle_sw,{0,0,0})
+
 local shuttle = Transform{
-	Model[[shuttle.ive]],
+	Model[[models/shuttle.ive]],
 	orientation = AngleAxis(Degrees(10), Axis{1.0, 0.0, 0.0}),
 	scale = 0.1,
 }
 shuttle = CenterTransformAtPosition(shuttle,{0,0,-10})
 
 local iss = Transform{
-	Model[[iss.ive]],
-	-- position = {5,0,-25},
+	Model[[models/iss.ive]],
 	orientation = AngleAxis(Degrees(20), Axis{1.0, 0.0, 0.0}),
-	scale = 100,
+	scale = 50,
 }
-iss = CenterTransformAtPosition(iss,{0,0,0})
+iss = CenterTransformAtPosition(iss,{5,10,-15})
 
 
--- RelativeTo.World:addChild(vortex)
-RelativeTo.World:addChild(soloarSystem)
+RelativeTo.World:addChild(vortex)
+RelativeTo.World:addChild(solarSystem)
 RelativeTo.World:addChild(shuttle)
+RelativeTo.World:addChild(shuttle_sw)
 RelativeTo.World:addChild(iss)
